4f0ad0d195a37e62a8a3d4157faf3bc0114d681a
abhishek17551 <ababhishek113@gmail.com>
2021-09-23 14:09:15 +0530

    until sum of date

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumberButton = document.querySelector("#check-number");
 
-checkNumberButton.addEventListener('click', function getvalues(){
-    console.log(dateOfBirth.value, luckyNumber.value);
-})
+
+function checkBirthDateLucky(){
+    const bdate = dateOfBirth.value;
+    const sum = calculateSum(bdate);
+    console.log(sum);
+}
+
+
+function calculateSum(bdate){
+   bdate = bdate.replaceAll("-","");
+   let sum=0;
+   for(i=0; i<bdate.length; i++){
+       sum = sum + Number(bdate.charAt(i)); //typecasting
+   }
+   return sum;
+}
+
+checkNumberButton.addEventListener('click',  checkBirthDateLucky)
